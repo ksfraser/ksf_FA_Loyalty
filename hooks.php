b23c68153d9723e007c5219bf10054bc9f2cfa53
Kevin Fraser <kevin@ksfraser.com>
2026-05-16 15:31:00 -0600

    Simplify hooks, update config to 2.4.3, move code to common utilities

--- hooks.php
+++ hooks.php
@@ -1,47 +1,56 @@
 <?php
 /**
- * FA_Loyalty Module Hooks for FrontAccounting
- * Customer Loyalty Program (depends on ksf_FA_CRM)
+ * ksf_FA_Loyalty Module Hooks for FrontAccounting
  */
 
-define('SS_LOYALTY', 118 << 8);
+define('SS_ksf_FA_Loyalty', 100 << 8);
 
-class hooks_fa_loyalty extends hooks {
-    var $module_name = 'fa_loyalty';
-    var $version = '1.0.0';
+class hooks_ksf_FA_Loyalty extends hooks {
+    var $module_name = 'ksf_FA_Loyalty';
 
-    function install_options($app) {
-        global $path_to_root;
-
-        switch($app->id) {
-            case 'CRM':
-                $app->add_rapp_function(0, _("Loyalty Program"),
-                    $path_to_root."/modules/".$this->module_name."/loyalty.php", 'SA_LOYALTYVIEW', MENU_ENTRY);
-                break;
-            case 'Sales':
-                $app->add_lapp_function(0, _("Customer Loyalty"),
-                    $path_to_root."/modules/".$this->module_name."/loyalty.php", 'SA_LOYALTYVIEW', MENU_ENTRY);
-                break;
-        }
+    function install_extension($check_only=true) {
+        return true;
     }
 
-    function install_access() {
-        $security_sections[SS_LOYALTY] = _("Loyalty Program");
-        $security_areas['SA_LOYALTYVIEW'] = array(SS_LOYALTY | 1, _("View Loyalty"));
-        $security_areas['SA_LOYALTYCREATE'] = array(SS_LOYALTY | 2, _("Manage Loyalty"));
-        return array($security_areas, $security_sections);
+    function install_tabs($app) {
+        // Override in modules that add apps
+    }
+
+    function install_options($app) {
+        // Override in modules that add menu items
     }
 
     function activate_extension($company, $check_only=true) {
-        $updates = array(
-            'sql/fa_customer_loyalty.sql' => array($this->module_name),
-            'sql/fa_loyalty_transactions.sql' => array($this->module_name)
-        );
-        return $this->update_databases($company, $updates, $check_only);
+        $this->ensure_composer_dependencies();
+        return true;
     }
 
-    function db_prevoid($trans_type, $trans_no) {
-        // Handle voiding if loyalty module tracks point transactions
+    function install_access() {
+        $security_sections[SS_ksf_FA_Loyalty] = _("");
+        $security_areas['SA_ksf_FA_LoyaltyVIEW'] = array(SS_ksf_FA_Loyalty | 1, _("View "));
+        $security_areas['SA_ksf_FA_LoyaltyMANAGE'] = array(SS_ksf_FA_Loyalty | 2, _("Manage "));
+        return array($security_areas, $security_sections);
+    }
+    
+    private function ensure_composer_dependencies() {
+        $module_dir = dirname(__FILE__);
+        $autoload_path = $module_dir . '/vendor/autoload.php';
+        
+        if (file_exists($autoload_path)) {
+            return;
+        }
+        
+        $composer_path = $module_dir . '/composer.json';
+        if (!file_exists($composer_path)) {
+            return;
+        }
+        
+        chdir($module_dir);
+        $output = [];
+        $return_code = 0;
+        exec('composer install --no-interaction --prefer-dist 2>&1', $output, $return_code);
+        if ($return_code !== 0) {
+            error_log('KSF Module: composer install failed: ' . implode("\n", $output));
+        }
     }
 }
-?>
